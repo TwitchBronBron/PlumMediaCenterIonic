57902ad65de8f887834f9215f871de108880428c
Bronley Plumb <bronley@gmail.com>
2017-08-24 12:56:48 -0400

    Added ability to reorder images during image compare.

--- src/components/image-list/image-list.ts
+++ src/components/image-list/image-list.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'image-list',
@@ -17,4 +17,34 @@ export class ImageListComponent {
         return this._urls;
     }
     private _urls: string[];
+
+    @Input()
+    public buttonType: 'add' | 'remove';
+
+    @Input()
+    public allowReorder: boolean;
+
+    @Output()
+    public buttonClick = new EventEmitter<string>();
+
+    public addRemoveClick(url: string) {
+        this.buttonClick.emit(url);
+    }
+
+    /**
+     * Move a url forward or backwards
+     * @param url 
+     * @param forward 
+     */
+    public reorder(url: string, forward = true) {
+        var idx = this.urls.indexOf(url);
+        var newIndex = idx + (forward ? 1 : -1);
+        if (newIndex >= this.urls.length) {
+            var k = newIndex - this.urls.length;
+            while ((k--) + 1) {
+                this.urls.push(undefined);
+            }
+        }
+        this.urls.splice(newIndex, 0, this.urls.splice(idx, 1)[0]);
+    }
 }
--- src/components/metadata-compare-row/metadata-compare-row.ts
+++ src/components/metadata-compare-row/metadata-compare-row.ts
@@ -18,4 +18,19 @@ export class MetadataCompareRowComponent {
 
     @Input()
     public comparison: MovieMetadataComparison;
+
+    public addImageUrl(propertyName, url) {
+        var arr: string[] = this.comparison.current[propertyName];
+        if (arr.indexOf(url) === -1) {
+            arr.push(url)
+        }
+    }
+
+    public removeImageUrl(propertyName, url) {
+        var arr: string[] = this.comparison.current[propertyName];
+        var idx = arr.indexOf(url);
+        if (idx > -1) {
+            arr.splice(idx, 1);
+        }
+    }
 }
