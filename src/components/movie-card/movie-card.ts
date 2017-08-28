261eb7691121e3a959d29e721ae2c1a27e6a3f4c
Bronley Plumb <bronley@gmail.com>
2017-08-28 07:15:48 -0400

    Added poster cache busting during development to help the correct posters show when regenerating the library

--- src/components/movie-card/movie-card.ts
+++ src/components/movie-card/movie-card.ts
@@ -8,10 +8,13 @@ import { Movie } from '../../interfaces/movie';
 export class MovieCardComponent {
 
     constructor() {
-
+        //during development, use an epoch time on querystring 
+        this.epoch = new Date().getTime();
     }
 
     @Input("movie")
     public movie: Movie;
+
+    private epoch: number;
 
 }
